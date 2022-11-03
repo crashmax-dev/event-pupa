af1232048bfddddf7bdb3a96cc85b143be03200d
Yurii Semenov <ysx080@gmail.com>
2022-11-03 21:50:59 +0500

    Added handle of termination signal

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,18 +5,30 @@ import (
 	"eventloop/pkg/api"
 	"fmt"
 	"go.uber.org/zap/zapcore"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 )
 
-func main() {
-	var quit chan struct{}
-	go api.StartServer(zapcore.InfoLevel, quit)
-	fmt.Println("Server started...")
+func inputMonitor(sc chan<- os.Signal) {
 	var input string
 	for strings.ToLower(input) != "stop" {
 		fmt.Scanln(&input)
 	}
+	sc <- syscall.SIGTERM
+}
+
+func main() {
+	quit := make(chan struct{})
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
+	go api.StartServer(zapcore.InfoLevel, &quit)
+	fmt.Println("Server started...")
+	go inputMonitor(sc)
+	<-sc
+	fmt.Println("Server stopping...")
 	api.StopServer(context.Background())
-	//<-quit
+	<-quit
 	fmt.Println("Server stopped.")
 }
