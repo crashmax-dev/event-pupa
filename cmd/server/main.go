efbd89d6e20647e1a1e098140b1040d32127820a
Yurii Semenov <ysx080@gmail.com>
2022-11-08 16:28:45 +0500

    Modified context in api starter, now it is with cancel

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -20,6 +20,9 @@ func inputMonitor(sc chan<- os.Signal) {
 }
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	quit := make(chan struct{})
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
@@ -28,7 +31,7 @@ func main() {
 	go inputMonitor(sc)
 	<-sc
 	fmt.Println("Server stopping...")
-	api.StopServer(context.Background())
+	api.StopServer(ctx)
 	<-quit
 	fmt.Println("Server stopped.")
 }
