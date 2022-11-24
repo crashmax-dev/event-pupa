c8868930fec1d67a49cc8ee0fdc40e711776e038
Yurii Semenov <ysx080@gmail.com>
2022-11-24 18:55:21 +0500

    feat: add success responses, comments

--- internal/httpApi/handler/scheduleHandler.go
+++ internal/httpApi/handler/scheduleHandler.go
@@ -2,14 +2,18 @@ package handlers
 
 import (
 	"context"
-	internal2 "eventloop/internal/http_api/internal"
+	"encoding/json"
+	"eventloop/internal/httpApi/eventpreset"
+	"eventloop/internal/httpApi/helper"
 	"io"
 	"net/http"
 	"strconv"
 	"strings"
 )
 
-// schedulerHandler запускает и останавливает выполнение интервальных событий, создаёт новые из пресетов/
+// schedulerHandler запускает и останавливает выполнение интервальных событий, создаёт новые из пресетов. При стопе
+// возвращает JSON вида:
+/* {Result: [...], Status: "..."}*/
 type schedulerHandler struct {
 	baseHandler
 }
@@ -26,41 +30,56 @@ func (sh *schedulerHandler) ServeHTTP(writer http.ResponseWriter, request *http.
 	ctx, _ := context.WithCancel(context.Background())
 
 	if request.Method != "POST" {
-		internal2.NoMethodResponse(writer, "POST")
-		sh.baseHandler.logger.Infof(internal2.ApiMessage("[Toggle] No such method: %s"), request.Method)
+		helper.NoMethodResponse(writer, "POST")
+		sh.baseHandler.logger.Infof(helper.ApiMessage("[Toggle] No such method: %s"), request.Method)
 		return
 	}
 
 	param := strings.TrimPrefix(request.URL.Path, "/scheduler/")
 
 	//Получаем ID ивента из URL, и создаём
-	if id, err := strconv.Atoi(param); err != nil {
-		internal2.ServerLogErr(writer, "no such event: %v", sh.baseHandler.logger, 400, param)
-		sh.logger.Debugf(internal2.ApiMessage("No such event details: %v"), err)
-	} else {
-		if newEvent, errC := internal2.CreateEvent(id, internal2.INTERVALED); errC != nil {
-			internal2.ServerLogErr(writer, "error while creating event: %v", sh.baseHandler.logger, 500, errC)
-		} else {
-			if errSE := sh.baseHandler.evLoop.ScheduleEvent(ctx, newEvent, nil); errSE != nil {
-				writer.WriteHeader(500)
-				sh.baseHandler.logger.Errorf(internal2.ApiMessage("schedule event fail: %v"), errSE)
+	if param != "" {
+		if id, err := strconv.Atoi(param); err == nil {
+			if newEvent, errC := eventpreset.CreateEvent(id, eventpreset.INTERVALED); errC == nil {
+				if errSE := sh.baseHandler.evLoop.ScheduleEvent(ctx, newEvent, nil); errSE == nil {
+					JSON.Status = "Event is scheduled succesfully"
+				} else {
+					writer.WriteHeader(500)
+					sh.baseHandler.logger.Errorf(helper.ApiMessage("schedule event fail: %v"), errSE)
+				}
+			} else {
+				helper.ServerLogErr(writer, "error while creating event: %v", sh.baseHandler.logger, 500, errC)
 			}
+		} else {
+			helper.ServerLogErr(writer, "no such event: %v", sh.baseHandler.logger, 400, param)
+			sh.logger.Debugf(helper.ApiMessage("No such event details: %v"), err)
 		}
 	}
 
 	if b, err := io.ReadAll(request.Body); err != nil {
-		internal2.ServerLogErr(writer, "bad request: %v", sh.baseHandler.logger, 400, err)
+		helper.ServerLogErr(writer, "bad request: %v", sh.baseHandler.logger, 400, err)
 	} else {
 		switch sm := strings.ToLower(string(b)); sm {
 		case "start":
 			if errSS := sh.baseHandler.evLoop.StartScheduler(ctx); errSS != nil {
-				writer.WriteHeader(500)
-				sh.baseHandler.logger.Errorf(internal2.ApiMessage("scheduler start fail: %v"), errSS)
+				if sh.baseHandler.evLoop.IsSchedulerRunning() {
+					writer.WriteHeader(400)
+					io.WriteString(writer, "Scheduler is already running")
+				} else {
+					writer.WriteHeader(500)
+				}
+				sh.baseHandler.logger.Errorf(helper.ApiMessage("scheduler start fail: %v"), errSS)
+			} else {
+				io.WriteString(writer, ". Scheduler started")
 			}
 		case "stop":
 			sh.baseHandler.evLoop.StopScheduler()
+			JSON = ScheduleResponse{Status: "Scheduler stopped", Result: sh.baseHandler.evLoop.GetSchedulerResults()}
+
 		default:
-			sh.baseHandler.logger.Errorf(internal2.ApiMessage("No known method: %v"), sm)
+			sh.baseHandler.logger.Errorf(helper.ApiMessage("No known method: %v"), sm)
 		}
 	}
+	byteJson, _ := json.Marshal(JSON)
+	writer.Write(byteJson)
 }
