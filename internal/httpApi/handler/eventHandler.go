b48377f5d05102609491b6c4580fcaa150ed6b24
Yurii Semenov <ysx080@gmail.com>
2022-11-24 20:47:08 +0500

    refactor: httpapi internal files moved

---
+++ internal/httpApi/handler/eventHandler.go
@@ -0,0 +1,92 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"eventloop/internal/httpApi/eventpreset"
+	"eventloop/internal/httpApi/helper"
+	"github.com/google/uuid"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+// eventHandler для обработки запросов по получению событий, по созданию и аттачу событий, удалению.
+// Событие создаётся из пресетов, по числу после "/events/"
+type eventHandler struct {
+	baseHandler
+}
+
+func (eh *eventHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	params := strings.Split(strings.TrimPrefix(request.URL.Path, "/events/"), "/")
+
+	switch request.Method {
+	case "GET":
+		eh.baseHandler.logger.Debugf(helper.ApiMessage("GET request"))
+		if evnts, err := eh.baseHandler.evLoop.GetAttachedEvents(params[0]); err == nil {
+			if codedMessage, errJson := json.Marshal(evnts); errJson == nil {
+				_, errW := writer.Write(codedMessage)
+				if errW != nil {
+					eh.baseHandler.logger.Errorf(helper.ApiMessage("error responding: %v"), errW)
+				}
+			} else {
+				helper.ServerLogErr(writer, errJson.Error(), eh.baseHandler.logger, 400)
+			}
+		} else {
+			helper.ServerLogErr(writer, err.Error(), eh.baseHandler.logger, 200)
+		}
+	case "POST", "PUT":
+		id, err := strconv.Atoi(params[0])
+		if err != nil || id > len(eventpreset.Events) || len(params) != 2 {
+			writer.WriteHeader(404)
+			return
+		}
+
+		eventName := params[1]
+		newEvent, err := eventpreset.CreateEvent(id, eventpreset.REGULAR)
+		if err != nil {
+			eh.baseHandler.logger.Errorf(helper.ApiMessage("Error while creating event: %v"), err)
+			return
+		}
+
+		errOn := eh.baseHandler.evLoop.On(ctx, eventName, newEvent, nil)
+		if errOn != nil {
+			eh.logger.Errorf(errOn.Error())
+			writer.WriteHeader(400)
+			io.WriteString(writer, "Event is not created")
+			return
+		}
+
+		eh.baseHandler.logger.Infof(helper.ApiMessage("Event type %v created for %v"), id, eventName)
+		_, err = io.WriteString(writer, newEvent.GetId().String())
+		if err != nil {
+			eh.baseHandler.logger.Errorf(helper.ApiMessage("error responding: %v"), err)
+		}
+	case "DELETE": //Удаление ивента
+		if b, err := io.ReadAll(request.Body); err == nil {
+			var sl []uuid.UUID
+			errJson := json.Unmarshal(b, &sl)
+			if errJson != nil {
+				helper.ServerLogErr(writer, "wrong json: %v", eh.baseHandler.logger, 400, errJson)
+				return
+			}
+			eh.baseHandler.logger.Infof(helper.ApiMessage("Removing events %v"), sl)
+			for _, v := range sl {
+				eh.baseHandler.evLoop.RemoveEvent(v)
+			}
+			_, errRespond := io.WriteString(writer, "OK")
+			if errRespond != nil {
+				eh.baseHandler.logger.Errorf(helper.ApiMessage("error responding: %v"), errRespond)
+			}
+
+		} else {
+			helper.ServerLogErr(writer, "Error reading request: %v", eh.baseHandler.logger, 400, err.Error())
+		}
+	default:
+		helper.NoMethodResponse(writer, "GET, POST, PUT, PATCH")
+	}
+}
---
+++ internal/httpApi/helpers_test.go
@@ -0,0 +1,144 @@
+package httpApi
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"eventloop/internal/httpApi/handler"
+	"fmt"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func readResponse(resp *http.Response) (result string, err error) {
+	scanner := bufio.NewScanner(resp.Body)
+	defer resp.Body.Close()
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		if result != "" {
+			result += " "
+		}
+		result += scanner.Text()
+	}
+	if err = scanner.Err(); err != nil {
+		return "", err
+	}
+	return result, nil
+}
+
+func handleRequest(t *testing.T, resp *http.Response, err error) string {
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, errResp := readResponse(resp)
+	if errResp != nil {
+		t.Log(errResp)
+	}
+	t.Log(result)
+	return result
+}
+
+func handleJsonRequest[T any](t *testing.T, resp *http.Response, err error) (result T) {
+	if err != nil {
+		t.Fatal(err)
+	}
+	errDecode := json.NewDecoder(resp.Body).Decode(&result)
+	if errDecode != nil {
+		t.Log(errDecode)
+	}
+	t.Log(result)
+	return
+}
+
+func createEvent(t *testing.T, eventName string) (*http.Response, string) {
+	requestURL := fmt.Sprintf("http://localhost:8090/events/1/%v", eventName)
+	resp, err := http.PostForm(requestURL, url.Values{})
+	return resp, handleRequest(t, resp, err)
+}
+
+func getEvents(t *testing.T, eventName string) (*http.Response, string) {
+	requestURL := fmt.Sprintf("http://localhost:8090/events/%v", eventName)
+	resp, err := http.Get(requestURL)
+	return resp, handleRequest(t, resp, err)
+}
+
+func triggerEvents(t *testing.T, eventName string) string {
+	requestURL := fmt.Sprintf("http://localhost:8090/trigger/%v", eventName)
+	resp, err := http.PostForm(requestURL, url.Values{})
+	return handleRequest(t, resp, err)
+}
+
+func toggleEvents(t *testing.T, body *bytes.Buffer) string {
+	resp, err := http.Post("http://localhost:8090/toggle/", "text/plain", body)
+	return handleRequest(t, resp, err)
+}
+
+func removeEvent(t *testing.T, ids []string) string {
+	client := &http.Client{}
+
+	encodedJson, errJson := json.Marshal(ids)
+	if errJson != nil {
+		t.Errorf("can't unmarshal json: %v", errJson)
+		return ""
+	}
+	reader := bytes.NewReader(encodedJson)
+	req, err := http.NewRequest("DELETE", "http://localhost:8090/events/", reader)
+	if err != nil {
+		t.Errorf("error creting request: %v", err)
+		return ""
+	}
+
+	// Fetch Request
+	resp, errDo := client.Do(req)
+	if errDo != nil {
+		t.Errorf("error fetching request: %v", errDo)
+		return errDo.Error()
+	}
+
+	// Read Response Body
+	result, errResp := readResponse(resp)
+	if errResp != nil {
+		t.Errorf("error reading response: %v", errResp)
+		return errResp.Error()
+	}
+
+	return result
+}
+
+func subscribeEvents(t *testing.T, eventName string, inputJSON struct {
+	Listeners []int `json:"listeners"`
+	Triggers  []int `json:"triggers"`
+}) string {
+	requestURL := fmt.Sprintf("http://localhost:8090/subscribe/%v", eventName)
+
+	encodedJSON, errJson := json.Marshal(inputJSON)
+	if errJson != nil {
+		t.Errorf("can't unmarshal json: %v", errJson)
+		return errJson.Error()
+	}
+
+	reader := bytes.NewReader(encodedJSON)
+
+	resp, errPost := http.Post(requestURL, "application/json", reader)
+	result := handleRequest(t, resp, errPost)
+
+	return result
+}
+
+func scheduleEvent(t *testing.T, body *bytes.Buffer) (*http.Response, string) {
+	resp, err := http.Post("http://localhost:8090/scheduler/1", "text/plain", body)
+	return resp, handleRequest(t, resp, err)
+}
+
+func scheduleStop(t *testing.T) (resp *http.Response, JSON handler.ScheduleResponse) {
+	var (
+		err error
+	)
+
+	data := bytes.NewBufferString("STOP")
+	resp, err = http.Post("http://localhost:8090/scheduler/", "text/plain", data)
+
+	JSON = handleJsonRequest[handler.ScheduleResponse](t, resp, err)
+
+	return
+}
