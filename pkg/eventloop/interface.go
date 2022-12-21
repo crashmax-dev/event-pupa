ebeb6936ca334767f77809f8fc0f0c6eec2b8b45
Yurii Semenov <ysx080@gmail.com>
2022-12-21 19:33:05 +0500

    fix: remove unused on, beforeevent and afterevent + interface actualize

--- pkg/eventloop/interface.go
+++ pkg/eventloop/interface.go
@@ -3,26 +3,18 @@ package eventloop
 import (
 	"context"
 
-	"eventloop/pkg/channelEx"
 	"eventloop/pkg/eventloop/event"
-	"eventloop/pkg/eventloop/internal/scheduler"
 	"github.com/google/uuid"
 )
 
 type Interface interface {
-	addEvent(eventName string, newEvent event.Interface)
-	On(ctx context.Context, eventName string, newEvent event.Interface, out chan<- uuid.UUID) error
-	OnBefore(ctx context.Context, eventName string, elFunction event.EventFunc)
-	OnAfter(ctx context.Context, eventName string, elFunction event.EventFunc)
-	Trigger(ctx context.Context, eventName string, ch channelEx.Interface[string]) error
+	RegisterEvent(ctx context.Context, newEvent event.Interface) error
+	Trigger(ctx context.Context, triggerName string) error
 	Toggle(eventFunc ...EventFunction) string
-	ScheduleEvent(ctx context.Context, newEvent event.Interface, out chan<- uuid.UUID) error
-	StartScheduler(ctx context.Context) error
-	Scheduler() scheduler.Interface
-	StopScheduler()
 	// RemoveEventByUUIDs удаляет событие по срезу идентификаторов. Возвращает срез оставшихся событий из запроса, которые
 	// не были удалены
 	RemoveEventByUUIDs(id []uuid.UUID) []uuid.UUID
 	Subscribe(ctx context.Context, triggers []event.Interface, listeners []event.Interface) error
 	GetAttachedEvents(eventName string) (result []uuid.UUID, err error)
+	Sync() error
 }
