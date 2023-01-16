6a7d0ffce6d6a5b719b1fac49a0a1f47c892bef0
Yurii Semenov <ysx080@gmail.com>
2023-01-16 19:33:15 +0500

    test: after

--- pkg/eventloop/event/after/after_test.go
+++ pkg/eventloop/event/after/after_test.go
@@ -1,13 +1,14 @@
 package after
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
 
 func Test_eventAfter_GetDurationSec(t *testing.T) {
 	type fields struct {
-		date DateAfter
+		date DateAfterArgs
 	}
 	want1, _ := time.ParseDuration("3s")
 	want2, _ := time.ParseDuration("1m")
@@ -19,15 +20,14 @@ func Test_eventAfter_GetDurationSec(t *testing.T) {
 	}{
 		{name: "Absolute",
 			fields: fields{
-				DateAfter{date: time.Now().Add(time.Second * 3)},
+				DateAfterArgs{Date: time.Now().Add(time.Second * 3)},
 			},
 			want: want1,
 		},
 		{
 			name: "Relative",
-			fields: fields{DateAfter{date: time.Date(1, 1, 1, 0, 1, 0, 0,
-				time.UTC),
-				isRelative: true}},
+			fields: fields{DateAfterArgs{Date: time.Time{}.Add(time.Minute),
+				IsRelative: true}},
 			want: want2,
 		},
 	}
@@ -38,6 +38,141 @@ func Test_eventAfter_GetDurationSec(t *testing.T) {
 			}
 			if got := e.GetDuration(); got != tt.want {
 				t.Errorf("GetDuration() = %v, want %v", got, tt.want)
+			} else {
+				t.Log(got)
+			}
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	var currentDate = time.Now()
+	tests := []struct {
+		name string
+		args DateAfterArgs
+		want Interface
+	}{
+		{
+			name: "Default",
+			args: DateAfterArgs{Date: currentDate.UTC(), IsRelative: true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := New(tt.args)
+			tt.want = &eventAfter{date: DateAfterArgs{Date: currentDate, IsRelative: true},
+				breakCh: got.GetBreakChannel()}
+			if got.IsDone() != tt.want.IsDone() && got.GetDuration() != tt.want.GetDuration() && got.GetBreakChannel() != got.GetBreakChannel() {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_eventAfter_GetBreakChannel(t *testing.T) {
+	var ch = make(chan bool)
+	type fields struct {
+		date    DateAfterArgs
+		breakCh chan bool
+		isDone  bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   chan bool
+	}{
+		{
+			name:   "Default",
+			fields: fields{date: DateAfterArgs{Date: time.Now()}, breakCh: ch},
+			want:   ch,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &eventAfter{
+				date:    tt.fields.date,
+				breakCh: tt.fields.breakCh,
+				isDone:  tt.fields.isDone,
+			}
+			if got := e.GetBreakChannel(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetBreakChannel() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_eventAfter_IsDone(t *testing.T) {
+	type fields struct {
+		date    DateAfterArgs
+		breakCh chan bool
+		isDone  bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{name: "Default",
+			fields: fields{date: DateAfterArgs{Date: time.Now()}, breakCh: make(chan bool), isDone: true},
+			want:   true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &eventAfter{
+				date:    tt.fields.date,
+				breakCh: tt.fields.breakCh,
+				isDone:  tt.fields.isDone,
+			}
+			if got := e.IsDone(); got != tt.want {
+				t.Errorf("IsDone() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_eventAfter_Wait(t *testing.T) {
+	type fields struct {
+		date    DateAfterArgs
+		breakCh chan bool
+		isDone  bool
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		breakFunc func(ch chan bool)
+	}{
+		{
+			name: "Absolute",
+			fields: fields{date: DateAfterArgs{Date: time.Now().Add(time.Millisecond * 20)},
+				breakCh: make(chan bool)},
+		},
+		{
+			name: "Relative",
+			fields: fields{date: DateAfterArgs{Date: time.Time{}.Add(time.Millisecond * 20), IsRelative: true},
+				breakCh: make(chan bool)},
+		},
+		{
+			name: "Break by channel",
+			fields: fields{date: DateAfterArgs{Date: time.Now().Add(time.Second * 3)},
+				breakCh: make(chan bool)},
+			breakFunc: func(ch chan bool) {
+				ch <- true
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &eventAfter{
+				date:    tt.fields.date,
+				breakCh: tt.fields.breakCh,
+				isDone:  tt.fields.isDone,
+			}
+			if tt.breakFunc != nil {
+				go tt.breakFunc(e.GetBreakChannel())
+			}
+			e.Wait()
+			if got := e.IsDone(); !got {
+				t.Errorf("IsDone() = %v, want %v", got, true)
 			}
 		})
 	}
